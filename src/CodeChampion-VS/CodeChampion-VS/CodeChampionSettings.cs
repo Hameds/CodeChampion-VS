e7d017b2ed13fdfc1d0a3c5ca09762004fe53fc8
Hamed <1385656+Hameds@users.noreply.github.com>
2016-09-13 23:24:39 +0430

    initial commit

---
+++ src/CodeChampion-VS/CodeChampion-VS/CodeChampionSettings.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChampion_VS
+{
+    public class CodeChampionSettings : DialogPage
+    {
+
+        [Category("CodeChampion Settings")]
+        [DisplayName("✌️ Epic Victory")]
+        [Description("Epic Victory Sound")]
+        public VictorySound EpicVictorySound
+        {
+            get;set;
+        }
+
+        [Category("CodeChampion Settings")]
+        [DisplayName("💩 Epic Fail")]
+        [Description("Epic Fail Sound")]
+        public FailSound EpicFailSound
+        {
+            get; set;
+        }
+
+
+
+        public enum VictorySound
+        {
+            [Description("sonic_the_hedgehog_act_cleared.wav")]
+            SonicTheHedgehog,
+            [Description("supermariobros_stage_clear.wav")]
+            SuperMarioBros,
+            [Description("duck_hunt_theme.wav")]
+            DuckHunt,
+            [Description("mk_finish_him.wav")]
+            MortalKombat_FinishHim,
+            [Description("mk_flawless_victory.wav")]
+            MortalKombat_FlawlessVictory,
+            [Description("mk_laugh.wav")]
+            MortalKombat_HaHaHa,
+            [Description("mitokomon.wav")]
+            Mitokomon,
+            [Description("this_is_sparta.wav")]
+            SPARTA,
+            [Description("starwars_theme.wav")]
+            StarWars,
+            [Description("eye_of_tiger.wav")]
+            EyeOfTiger,
+            [Description("we_will_rock_you.wav")]
+            WeWillRockYou,
+            [Description("gangnam_style.wav")]
+            GangnamStyle,
+            [Description("battle_without_honor_or_humanity.wav")]
+            KillBill,
+            [Description("livin_la_vida_loca.wav")]
+            LivinLaVidaLoca,
+            [Description("la_copa_de_lavida.wav")]
+            LaCopaDeLavida,
+            [Description("o_fortuna.wav")]
+            OFortuna,
+            [Description("symphony_n5.wav")]
+            SymphonyN5,
+            [Description("iran_national_anthem.wav")]
+            IranNationalAnthem,
+            [Description("mellipoushan.wav")]
+            Mellipoushan
+        }
+
+
+        public enum FailSound
+        {
+            [Description("supermariobros_gameover.wav")]
+            SuperMarioBros,
+            [Description("duck_hunt_laugh.wav")]
+            DuckHunt,
+            [Description("homer_you_tried.wav")]
+            HomerSimpson,
+            [Description("pulp_fiction_say_what_again.wav")]
+            PulpFiction,
+            [Description("darth_vader_dont_fail_me.wav")]
+            DarthVader,
+            [Description("voldemort_your_a_fool.wav")]
+            LordVoldemort,
+            [Description("arrow_you_have_failed.wav")]
+            Arrow,
+            [Description("a_lannister_always_pays_his_debts.wav")]
+            GOT,
+            [Description("chop_suey.wav")]
+            ChopSuey,
+            [Description("someone_like_you.wav")]
+            Adele
+        }
+    }
+}
---
+++ src/CodeChampion-VS/CodeChampion-VS/CodeChampionVictory.cs
@@ -0,0 +1,109 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="CodeChampionVictory.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System.Media;
+using static CodeChampion_VS.CodeChampionSettings;
+using System.IO;
+using System.Reflection;
+
+namespace CodeChampion_VS
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class CodeChampionVictory
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("55568db0-7a8b-43c4-af05-f9ea1bed4378");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CodeChampionVictory"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private CodeChampionVictory(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static CodeChampionVictory Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new CodeChampionVictory(package);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            CodeChampionPackage codeChampionPackage = this.package as CodeChampionPackage;
+            VictorySound userSound = codeChampionPackage.UserVictorySound;
+            string epicSoundPath = String.Format("{0}/Resources/sounds/win/{1}", Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), Utils.ToDescriptionString(userSound));
+            using (SoundPlayer player = new SoundPlayer(epicSoundPath))
+            {
+                player.Play();
+            }
+        }
+    }
+}
